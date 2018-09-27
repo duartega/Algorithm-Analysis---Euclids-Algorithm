293f3001b8737e129a304ca6efe2390ac081caad
gabriel <duartega@sonoma.edu>
2018-09-26 20:53:59 -0700

    Updated python script to current cpp status

--- Project1.py
+++ Project1.py
@@ -16,23 +16,45 @@ def euclids_algo(m,n, avg, count):
     else:
         gcd = int(m) % int(n) # gets the remainder
         count += 1
-        #avg += int(p)
-        #print('avg:',avg)
+
         if (gcd == 0):        # the base case
-            print('count:',count)
+            print('Number of modulo divisions:',count)
             return n
         else:
             #avg += int(n)
             return euclids_algo(n, gcd, avg, count)
 
-def consec_int_checking(m,n):
-    t = m # {m,n}
-    if (int(m) / int(t) == 0):
-        if (int(t) == int(n) / int(t)):
-            return int(t)
-        else:
-            t-=t
-    print(t)
+def consec_int_checking(m,n, count):
+    
+    
+    temp = 0.0
+    temp2 = 1.0
+    t = 0.0
+    original_n = int(n)
+    
+    if (int(n) > int(m)):
+        swap = int(n)
+        n = int(m)
+        m = swap
+        original_n = int(n)
+    
+    if (int(n) < int(m)):
+        while (int(temp2) != 0.0):
+            t = int(n)
+            temp = int(m)%int(t)
+            count += 1
+            if (int(temp)==0.0):
+                temp2 = int(original_n)%int(t)
+                count += 1
+                if (int(temp2) == 0.0 and int(temp) == int(temp2)):
+                    print("Number of modulo divisions:",count)
+                    return t
+                else:
+                    n = int(n) - int(1.0)
+            else:
+                n = int(n) - int(1.0)
+                    
+
 
 def main():
     avg = 0
@@ -40,10 +62,10 @@ def main():
     eu_al_m = input("Please enter an integer for m: ")
     eu_al_n = input("Please enter an integer for n: ")
 
-    out = euclids_algo(eu_al_m, eu_al_n, avg, count) # send the two numbers to the function
+    out = euclids_algo(eu_al_m, eu_al_n, avg, count) # send the two numbers to the f
     print('GCD:', out)
 
-    #consec_int_checking(eu_al_m, eu_al_n)
-
+    out = consec_int_checking(eu_al_m, eu_al_n, count)
+    print("Consecutive GCD:", out)
 
 main()

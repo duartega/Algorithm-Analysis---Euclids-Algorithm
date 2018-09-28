1d3250da45c5a805473f36c6c1e1c67a5e8e3045
Elliot Newman <elliotn46@gmail.com>
2018-09-27 18:48:04 -0700

    both avg working

--- Project1.cpp
+++ Project1.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 int global_count = 0;
+int global_count2 = 0;
 
 
 int euclids_ago(int m, int n, int count, int done)
@@ -19,7 +20,7 @@ int euclids_ago(int m, int n, int count, int done)
 
                 if (gcd == 0)
                 {
-                        if (done == 10) {
+                        if (done == 0) {
                                 cout << "Count: " << count << endl;
                                 global_count = count;
                         }
@@ -27,18 +28,68 @@ int euclids_ago(int m, int n, int count, int done)
                 }
                 else
                 {
-                        if (done == 10)
-                                euclids_ago(n, gcd, count, 10);
+                        if (done == 0)
+                                euclids_ago(n, gcd, count, 0);
                         else
-                                euclids_ago(n, gcd, count, 11);
+                                euclids_ago(n, gcd, count, 1);
                 }
         }
 }
 
-float calc_avg(int sCount)
+int consec_int_checking(int m, int n, int count, int done)
+{
+
+        int temp = 0;
+        int temp2 = 1;
+        int t = 0;
+        int original_n = n;
+
+        if (n > m)
+        {
+                int swap = 0;
+                swap = n;
+                n = m;
+                m = swap;
+                original_n = n;
+        }
+
+        if (n < m)
+        {
+
+                while (temp2 != 0)
+                {
+                        t = n;
+                        temp = m%t;
+                        if (temp == 0)
+                        {
+                                temp2 = original_n%t;
+                                if (temp2 == 0 && temp == temp2)
+                                {
+                                        if(done == 0)
+                                        {
+                                                cout << "T= " << t << endl;
+                                                global_count2 = t;
+                                                cout << global_count2 << endl;
+                                        }
+                                        return t;
+                                }
+                                else
+                                {
+                                        n--;
+                                }
+                        }
+                        else
+                                n--;
+                }
+        }
+}
+
+
+
+float euclids_avg(int sCount)
 {
         int array[sCount];
-        int rec = 11;
+        int rec = 1;
         for(int i=0; i < sCount; i++)
         {
                 array[i] = euclids_ago(sCount, i, 0, rec);
@@ -58,56 +109,31 @@ float calc_avg(int sCount)
 
 }
 
-int consec_int_checking(int m, int n, int count)
+
+float consec_avg(int sCount)
 {
-        int temp = 0;
-        int temp2 = 0;
-        int t = 0;
-	int original_n = n;
+        int array[sCount];
+        int rec = 1;
+        for(int i=0; i < sCount; i++)
+        {
+                array[i] = consec_int_checking(sCount, i+1, 0, rec);
+                rec++;
 
-	if (n == 0 || m == 0)
-	  return 0;
 
-	if (n == m)
-	  return m;
 
-	if (n > m)
-	  {
-	    int swap = n;
-	    n = m;
-	    m = swap;
-	    original_n = n;
-	  }
+        }
+        float avg = 0.0;
+        int total = 0;
 
-	if (n < m)
-	  {
-	    while (temp2 != 0)
-	      {
-		t = n;
-		temp = m % t;
-		count += 1;
-		
-		if (temp == 0)
-		  {
-		    temp2 = original_n % t;
-		    count += 1;
-		    
-		    if (temp2 == 0 && temp == temp2)
-		      {
-			cout << "Number of modulo divisions: " << count << endl;
-			return t;
-		      }
-		    else
-		      {
-			n -= 1;
-		      }
-		  }
-		else
-		  {
-		    n -= 1;
-		  }
-	      }
-	  }
+        for (int j = 0; j < sCount; j++)
+        {
+                total +=array[j];
+        }
+
+        avg = (float)total / (float)sCount;
+
+        return avg;
+
 }
 
 
@@ -126,16 +152,21 @@ int main() {
         cin >> eu_al_n;
         cout << endl;
 
-        out = euclids_ago(eu_al_m, eu_al_n, count, 10);
+        out = euclids_ago(eu_al_m, eu_al_n, count, 0);
 
         cout << "GCD: " << out << endl;
 
-        float output2 = calc_avg(global_count);
-        cout << "Average is: " << output2 << endl;
+        float output2 = euclids_avg(global_count);
+        cout << "Average of Euclid's Algoithm is: " << output2 << endl;
 
-    
-	out = consec_int_checking(eu_al_m, eu_al_n, count);
-	cout << "Consecutive GCD: " << out << endl;
-	
+        int output3 = consec_int_checking(eu_al_m,eu_al_n,count, 0);
+
+        float output4 = consec_avg(global_count);
+        cout << "Average of Consecutive Integers is: " << output4 << endl;
+
+
+        out = consec_int_checking(eu_al_m, eu_al_n, count);
+        cout << "Consecutive GCD: " << out << endl;
+
         return 0;
 }

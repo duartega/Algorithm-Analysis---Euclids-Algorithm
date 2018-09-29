0569401cb761f59935a0d987289e86feb8b5ca96
gabriel <duartega@sonoma.edu>
2018-09-28 21:59:22 -0700

    Almost done. Want to have a backup just in case

--- Project1.py
+++ Project1.py
@@ -8,13 +8,11 @@ Authors : Gabriel Duarte, Elliot Newman
 #from __future__ import print_function
 #import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
 import numpy as np
 import math
 import time
-
-global_count = int(0)
-global_count2 = int(0)
-fib_count = int(0)
+import random
 
 
 def euclids_algo(m, n, count, done):
@@ -29,15 +27,9 @@ def euclids_algo(m, n, count, done):
 
         # Sets the base case for the GCD
         if (gcd == 0):
-            if (done == int(10)):
-                
-                global global_count
-                global fib_count
-                global_count = int(count)
-                if (fib_count == 0):
-                    print('\nNumber of modulo divisions:',count)
-                fib_count = int(count)
-            return n
+            #if (done == int(10)):
+                #print('\nNumber of modulo divisions:',count)
+            return count
         else:
             if (done == int(10)):
                 return euclids_algo(n, gcd, count, int(10))
@@ -85,19 +77,15 @@ def consec_int_checking(m,n, count, done):
                     
                     if (done == 10):
                         print("\nNumber of modulo divisions:",count)
-                        global global_count2
-                        global_count2 = int(count)
-                    return t
+                    return count
                 else:
                     n = int(n) - int(1.0)
             else:
                 n = int(n) - int(1.0)
 
-def Fib():
-    print("Starting timer")
-    start = time.time()
+def Fib(x, n):
+
     # Im still working on this
-    x = int(input("\nWhat position in the Fibonacci sequence would you like to see up to? "))
     arr = []
     arr.append(int(0)) # This is for arr[0] = 1
     arr.append(int(1)) # This is for arr[1] = 1
@@ -107,16 +95,16 @@ def Fib():
         print(arr[i], end=' ')
 
     count = int(0)
+    ret_count = int(0)
 
     #GCD(m,n) qhwew m = F(k+1) and n = F(k) for k > 1
     for i in range(1, len(arr)-1):
-        euclids_algo(arr[i], arr[i-1], count, int(10))
-    print("\nNumber of modulo divisions for (", arr[-2], ", ", arr[-3], ") is:", fib_count)
-    end = time.time()
-    time_count = float(end - start)
-    avg = float(calc_avg(fib_count, 3, time_count))
- 
-
+        ret_count = euclids_algo(arr[i], arr[i-1], count, int(10))
+    #print("\nNumber of modulo divisions for (", arr[-2], ", ", arr[-3], ") is:", fib_count)
+    if (n == 1):
+        return ret_count
+    if (n == 2):
+        return arr
 def sieve(n):
 
     # Initialize the arrays
@@ -151,7 +139,7 @@ def sieve(n):
 def calc_avg(sCount, which_plot, time_count):
 
     arr = []
-    rec = 11
+    rec = 10
 
     if (sCount == int(0)):
         return 0
@@ -165,7 +153,6 @@ def calc_avg(sCount, which_plot, time_count):
             arr.append(consec_int_checking(sCount, i, 0, rec))
             rec += 1
     elif (which_plot == 3):
-        start = time.time()
         for i in range(sCount):
             arr.append(euclids_algo(sCount, i, 0, rec))
             rec += 1
@@ -174,18 +161,8 @@ def calc_avg(sCount, which_plot, time_count):
 
     for i, _ in enumerate(arr):
         total += arr[i]
-        #print("arr[i]:",arr[i])
 
     avg = total / sCount
-    print("Average is:", avg)
-    if (which_plot == 3):
-        end = time.time()
-        time_count += float(end-start)
-    print("Run time:", time_count)
-
-    print("Loading Scatterplot...")
-    scatter_plot(arr, avg, sCount, which_plot)
-
     return avg
 
 def scatter_plot(arr, avg, sCount, n):
@@ -195,46 +172,144 @@ def scatter_plot(arr, avg, sCount, n):
     ax.grid(True, zorder=0)
     ax.set_ylabel('GCD at iteration "i"')
     ax.set_xlabel('Iterations')
+    
     for i, _ in enumerate(arr):
-        x = plt.scatter(i, arr[i], c='b', marker='o')
-    y = ax.hlines(avg, 0, avg*sCount, colors='r', linestyles='dashed', label='Average')
+        x = plt.scatter(i, arr[i], c='b', marker='x')
+    #y = ax.hlines(avg, 0, avg*sCount, colors='r', linestyles='dashed', label='Average')
     
     if (n == 1):
         ax.set_title('MDavg(n) Scatterplot')
-        ax.legend((y,x),('MDavg(n)','GCD'),loc='upper left')
+        #ax.legend((y,x),('MDavg(n)','GCD'),loc='upper left')
     elif (n == 2):
         ax.set_title('Davg(n) Scatterplot')
-        ax.legend((y,x),('Davg(n)','GCD'),loc='upper left')
+        #ax.legend((y,x),('Davg(n)','GCD'),loc='upper left')
     elif (n == 3):
         ax.set_title('MDworst(n) Scatterplot')
-        ax.legend((y,x),('MDworst(n)','GCD'),loc='upper left')
+        #ax.legend((y,x),('MDworst(n)','GCD'),loc='upper left')
 
     plt.show()
 
+def common_elements():
+    start = int(0)
+    end = int(10)
+
+    inp = int(input("How many random numbers do you want to fill the list with to find common elements?"))
+
+    A = []
+    B = []
+    C = []
+
+    for i in range(inp):
+        A.append(random.randint(start,end))
+    for i in range(inp):
+        B.append(random.randint(start,end))
+
+    A.sort()
+    B.sort()
+
+    #A =  [2, 5, 5, 5, 15]
+
+
+    #B = [2, 2, 3, 5, 5, 7, 15, 20]
+
+
+    print("A:",A,"\nB:",B)
+
+    
+    i = int(0)
+    j = int(0)
+    while((i != len(A)-1)  or (j != len(B)-1)):
+        #---------------------------------------------
+        if (i == len(A)-1):
+            #print("Final I:", A[i], " ", B[j])
+            if (A[-1] == B[j]):
+                C.append(A[-1])
+                A[-1] = int(0)
+                break
+        if (j == len(B)-1):
+            #print("Final J:", A[i], " ", B[j])
+            if (B[-1] == A[i]):
+                C.append(B[-1])
+                B[-1] = int(0)
+                break
+        #---------------------------------------------
+        if (int(A[i]) == int(B[j]) and (i+1 != None or j+1 != None)):
+            C.append(A[i])
+            if (i != len(A) or j != len(B)):
+                i += int(1)
+                j += int(1)
+                #print("A[i]:",A[i], " B[j]:",B[j])
+                #print("I:",i, " J:",j)
+            continue
+        if (int(A[i]) > int(B[j])):
+            if (j != len(B)-1):
+                j += int(1)
+            else:
+                break
+            #print("A2[i]:",A[i], " B2[j]:",B[j])
+            #print("I:",i, " J:",j)
+        if (int(A[i]) < int(B[j])):
+            if (i != len(A)-1):
+                i += int(1)
+            else:
+                break
+            #print("A3[i]:",A[i], " B3[j]:",B[j])
+            #print("I:",i, " J:",j)
+
+        if (i == len(A)-1 and A[-1] <= B[j]):
+            continue
+        if (j == len(B)-1 and B[-1] <= A[i]):
+            continue
+
+    if (A[-1] == B[-1]):
+        C.append(A[-1])
+    #C = list((Counter(A) & Counter(B)).elements())
+    print("C:",C)
+
+    
 
 def main():
-    avg = 0
-    count = 0
-    eu_al_m = input("Please enter an integer for m: ")
-    eu_al_n = input("Please enter an integer for n: ")
 
-    print("\nEuclid's Algorithm:\n------------------------------")
-    out = euclids_algo(eu_al_m, eu_al_n, count, int(10)) # send the two numbers to the f
-    print('GCD:', out)
-    out2 = float(calc_avg(global_count, 1, 0))
+    end = int(100)
+    newarray = []
+    newarray2 = []
+    newarray3 = []
 
-    print("\nConsecutive Checking Algorithm:\n------------------------------")
-    out = consec_int_checking(eu_al_m, eu_al_n, count, int(10))
-    print('GCD:', out)
-    out3 = float(calc_avg(global_count2, 2, 0))
-  
+    for i in range(0, end, 5):
+        out2 = float(calc_avg(i, 1, 0))
+        newarray.append(out2)
 
-   
+    x = input("Would you like to see the scatter plot? (y/n)")
+    if (x == 'y'):
+        scatter_plot(newarray, newarray, end, int(1))
+        
 
-    Fib()
+    for i in range(0, end, 5):
+        out3 = float(calc_avg(i, 2, 0))
+        newarray2.append(out3)
+    x = input("Would you like to see the scatter plot? (y/n)")
+    if (x == 'y'):
+        scatter_plot(newarray2, newarray2, end, int(2))
 
 
-    inp = int(input("\nPlease enter a number to find its primes: "))
-    sieve(inp)
+    x = int(input("\nWhat position in the Fibonacci sequence would you like to see up to? "))
+    start = time.time()
+    ret_count = Fib(int(x), int(1))
+
+    for i in range(0, x, 5):
+        out4 = float(calc_avg(i, 3, 0))
+        newarray3.append(out4)
+    end = time.time()
+    print("\nTime taken to run Fib Seq:",end-start)
+    x = input("Would you like to see the scatter plot? (y/n)")
+
+    if (x == 'y'):
+        scatter_plot(newarray3, newarray3, ret_count, int(3))
+    
+    common_elements()
+
+
+   # inp = int(input("\nPlease enter a number to find its primes: "))
+   # sieve(inp)
 
 main()
